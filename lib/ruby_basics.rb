80c53f46b19856a83185923aad98f09234567c87
Parmanand Sahadeo <parmsahadeo530@gmail.com>
2016-06-07 19:04:01 +0000

    passed all tests from todo

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+  quo = num1 / num2
 end
 
 def assign_variable(value)
-  
+  variable = value
 end
 
-def argue
-  
+def argue(phrase)
+  phrase
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-
+  phrase = "Nice"
 end
 
 def last_evaluated_value
-
+  phrase = "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  topping
 end
